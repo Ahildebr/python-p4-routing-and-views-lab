8922fb3e984ad17acdc153023b1964440dd20c9e
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2025-02-17 15:49:35 -0800

    Partially completes Lab

--- server/app.py
+++ server/app.py
@@ -6,3 +6,20 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<string:text>")
+def print_string(text):
+    print(text)
+    return text
+
+@app.route("/count/<int:number>")
+def count(number):
+    numbers = "\n".join(str(i) for i in range(number)) + "\n"
+    print(numbers)
+    return numbers
